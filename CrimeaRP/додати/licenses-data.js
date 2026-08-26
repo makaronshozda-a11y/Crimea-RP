c9d07aa04b71c8b4facad87ca817f7d523548eda
makaronshozda-a11y <makaronshozda@gmail.com>
2026-08-26 18:44:12 +0300

    Add files via upload

---
+++ CrimeaRP/додати/licenses-data.js
@@ -0,0 +1,87 @@
+const licensesData = [
+
+
+  // ==================== ЛІЦЕНЗІЇ НА ЗБРОЮ ====================
+
+    {
+     number: "03398",
+     type: "weapon-storage",
+     nick: "Caston08963",
+     validFrom: "20.08.2026",
+     validTo: "20.08.2027"
+   },
+
+   {
+     number: "03785",
+     type: "weapon-storage",           
+     nick: "Yezh1lya",
+     validFrom: "20.07.2026",
+     validTo: "Нескінченно"
+   },
+   {
+     number: "03926",
+     type: "weapon-carry",           
+     nick: "Yezh1lya",
+     validFrom: "20.07.2026",
+     validTo: "Нескінченно"
+   },
+   {
+     number: "03755",
+     type: "weapon-storage",           
+     nick: "pravolevo678",
+     validFrom: "19.08.2026",
+     validTo: "19.08.2027"
+   },
+   {
+     number: "04855",
+     type: "weapon-carry",           
+     nick: "pravolevo678",
+     validFrom: "19.08.2026",
+     validTo: "19.08.2027"
+   },
+
+
+  // ==================== ЛІЦЕНЗІЇ НА ТАКСІ ====================
+
+   {
+     number: "18434",
+     type: "taxi",           
+     nick: "Matviy105012",
+     validFrom: "20.07.2026",
+     validTo: "Нескінченно"
+   },
+
+
+      // ==================== ЛІЦЕНЗІЇ АДВОКАТА ====================
+
+   {
+     number: "20498",
+     type: "advocate",           
+     nick: "john46409",
+     validFrom: "18.08.2026",
+     validTo: "18.08.2027"
+   },
+   {
+     number: "22448",
+     type: "advocate",           
+     nick: "agency_roblox4",
+     validFrom: "19.08.2026",
+     validTo: "18.08.2027"
+   },
+   {
+     number: "29585",
+     type: "advocate",           
+     nick: "Zoomsos123",
+     validFrom: "24.08.2026",
+     validTo: "24.08.2027"
+   },
+
+];
+
+
+const licenseTypeNames = {
+  "advocate": "Ліцензія адвоката",
+  "taxi": "Ліцензія таксиста",
+  "weapon-storage": "Ліцензія на зброю (зберігання)",
+  "weapon-carry": "Ліцензія на приховане носіння зброї"
+};
---
+++ CrimeaRP/додати/staff-data.js
@@ -0,0 +1,152 @@
+const staffData = {
+
+  // ==================== АДМІНІСТРАЦІЯ ====================
+  administration: {
+    title: "Адміністрація",
+    members: [
+
+        {
+         name: "Власник Сервера ",
+         nick: "Yezh1lya",
+         telegram: "https://t.me/Absolute_WT",
+         avatar: "https://i.postimg.cc/fbxhXRhx/image-(6).webp"
+        },
+
+    ]
+  },
+
+  // ==================== СУД (СВУ) ====================
+  court: {
+    title: "Судова влада України (СВУ)",
+    members: [
+
+       {
+         name: "Головний суддя СВУ",
+         nick: "Yar20269",
+         telegram: "https://t.me/manageryarikbot", 
+         avatar: "https://i.postimg.cc/Zqf3LHLv/image-(10).webp"
+       },
+
+    ]
+  },
+
+  // ==================== ДБР ====================
+  dbr: {
+    title: "Державне бюро розслідувань (ДБР)",
+    members: [
+
+    ]
+  },
+
+  // ==================== НПУ ====================
+  npu: {
+    title: "Національна поліція України (НПУ)",
+    members: [
+
+       {
+         name: "Начальник НПУ",
+         nick: "pravolevo678",
+         telegram: "https://t.me/Erorse",
+         avatar: "https://i.postimg.cc/5tvf3m82/image-(5).webp"
+       },
+       {
+         name: "Голова ГСУ",
+         nick: "Yezh1lya",
+         telegram: "https://t.me/Absolute_WT",
+         avatar: "https://i.postimg.cc/fbxhXRhx/image-(6).webp"
+       },
+       {
+         name: "Командир КОРД",
+         nick: "DANYA0705DANYA",
+         telegram: "https://t.me/xXz8br8Xx",
+         avatar: "https://i.postimg.cc/T3sq244q/image-(9).webp"
+       },
+       {
+         name: "Заступник командира КОРД",
+         nick: "samunzhiev",
+         telegram: "https://t.me/LevBarinovv",
+         avatar: "https://i.postimg.cc/tCCh3b0S/image-(10).webp"
+       },
+       {
+         name: "Голова департаменту патрульної полції",
+         nick: "starikashka228337",
+         telegram: "https://t.me/xxx_DELTA_xxx",
+         avatar: "https://i.postimg.cc/0jf7H9zj/image-(13).webp"
+       },
+
+
+    ]
+  },
+
+  // ==================== ПРОКУРАТУРА ====================
+  prosecution: {
+    title: "Прокуратура",
+    members: [
+
+      {
+        name: "Генеральний прокурор",
+         nick: "Zoomsos123",
+         telegram: "https://t.me/kitsuxen",
+         avatar: "https://i.postimg.cc/Y0QgBgcx/image-(3).webp"
+       },     
+       
+    ]
+  },
+
+     // ==================== ДСНС ====================
+    dsns: {
+     title: "ДСНС",
+      members: [
+
+      {
+        name: "Директор ДСНС",
+         nick: "agency_roblox4",
+         telegram: "https://t.me/tfeqohe",
+         avatar: "https://i.postimg.cc/90yXQYQj/no-Filter.webp"
+       },  
+       {
+        name: "Головний рятувальник",
+         nick: "Matviy105012",
+         telegram: "https://t.me/NOKIA3310_matvii",
+         avatar: "https://i.postimg.cc/J4qBfZLz/image.webp"
+       },
+       {
+        name: "Рятувальник II класу",
+         nick: "jehfjrgrivbrhtklf",
+         telegram: "https://t.me/jehfirgrivbrhtklf",
+         avatar: "https://i.postimg.cc/jddjJ1pd/image-(1).webp"
+       },   
+       
+    ]
+  },
+       // ==================== Інше ====================
+    another: {
+     title: "Інше",
+      members: [
+
+       {
+        name: "Технічний Адміністратор Сайта",
+         nick: "Zoomsos123",
+         telegram: "https://t.me/kitsuxen",
+         avatar: "https://i.postimg.cc/Y0QgBgcx/image-(3).webp"
+       },   
+       {
+        name: "Відеооператор",
+         nick: "Levopravo2024",
+         telegram: "https://t.me/Alexandriulik",
+         avatar: "https://i.postimg.cc/02HvwCLn/image-(4).webp"
+       }, 
+       {
+        name: "Відеооператор",
+         nick: "fnpldf_5",
+         telegram: "https://t.me/hello_amqruko",
+         avatar: "https://i.postimg.cc/PqChMfpx/image-(5).webp"
+       },     
+
+       
+    ]
+  },
+
+
+
+};
