2a6518a949711af26d4dfa18f691a22758919124
makaronshozda-a11y <makaronshozda@gmail.com>
2026-08-27 14:05:40 +0300

    Add files via upload

---
+++ Lviv Region RP/додавати/roles.js
@@ -0,0 +1,40 @@
+
+const staffData = {
+
+  administration: {
+    title: "Адміністрація",
+    members: [
+
+      {
+        name: "Власник Сервера",
+        nick: "YourNick",
+        telegram: "https://t.me/yourlink",
+        avatar: "https://i.postimg.cc/приклад.webp"
+      },
+
+    ]
+  },
+
+  court: {
+    title: "Суд",
+    members: [
+
+    ]
+  },
+
+    SBI: {
+    title: "ДБР",
+    members: [
+
+    ]
+  },
+
+    SBS: {
+    title: "СБС",
+    members: [
+
+    ]
+  },
+
+
+};
